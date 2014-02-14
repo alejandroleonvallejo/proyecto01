9cf9d670ac2a5f40c43503228a20cd7f686c21dc
ALEX LEON VALLEJO <alvale_555@gmail.com>
2014-02-14 12:29:15 -0600

    version 3
    tercera prueba del programa

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     document.addEventListener("deviceready",function () {
 		$('#B1').tap(function (){
 		navigator.notificacion.beep(1);
-		
-		});//tap b1
+			});//tap b1
+			$('#B2').tap(function (){
+		navigator.notificacion.vibrate(100);
+			});//tap b2
 	}); //devide ready
 });//ready 
